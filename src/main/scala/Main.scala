256b82a52fc8de561d7d72e6b615cc5d8b7d43de
Administrator <admin@example.com>
2015-03-17 23:27:45 +0900

    init commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,14 @@
+import org.atilika.kuromoji.{Token, Tokenizer}
+
+/**
+ * Created by motonari on 15/03/17.
+ */
+object Main {
+  val tokenizer = Tokenizer.builder.mode(Tokenizer.Mode.NORMAL).build
+
+  val tokens = this.tokenizer.tokenize("今日はいい天気ですね。").toArray
+
+  def main(args: Array[String]) {
+    tokens.map( s => println( s.asInstanceOf[Token].getSurfaceForm ) )
+  }
+}
